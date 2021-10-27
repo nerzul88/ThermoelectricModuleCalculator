6c3987709bf10f2d8d7d3517e9024cc827f2a480
nerzul88 <nerzul.88@gmail.com>
2021-10-27 22:22:17 +0300

    Constraints updated

--- ThermoelectricModuleCalculator/MainViewController.swift
+++ ThermoelectricModuleCalculator/MainViewController.swift
@@ -128,19 +128,23 @@ class MainViewController: UIViewController {
         view.addSubview(circleButton(button: ceramicCalculateButton, imagePath: "BlueScreen"))
         
         //скрываем клавиатуру
-        self.hideKeyboardWhenTappedAround()
+        //self.hideKeyboardWhenTappedAround()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 }
 //скрытие клавиатуры после нажатия на пустое место на экране
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
+//extension UIViewController {
+//    func hideKeyboardWhenTappedAround() {
+//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+//        tap.cancelsTouchesInView = false
+//        view.addGestureRecognizer(tap)
+//    }
+//
+//    @objc func dismissKeyboard() {
+//        view.endEditing(true)
+//    }
+//}
